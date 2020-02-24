6ef75230cfdffa9ef6de0642f893bb7d3fc4c236
Vyacheslav <vyacheslav.isupov@catenamedia.com>
2020-02-24 10:28:19 +0200

    Added main functionality (adding the tasks)

---
+++ src/TodoList.js
@@ -0,0 +1,53 @@
+import React, {Component} from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
+
+class TodoList extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+    addItem(e) {
+        var itemArray = this.state.items;
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift({
+                text: this._inputElement.value,
+                key: Date.now()
+            });
+
+            this.setState({
+                items: itemArray
+            });
+
+            this._inputElement.value = "";
+        }
+
+        e.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input
+                            ref={(a) => this._inputElement = a}
+                            placeholder="Enter new task">
+                        </input>
+                        <button type="submit">ok</button>
+                    </form>
+                </div>
+                <TodoItems entries={this.state.items} />
+            </div>
+        );
+    }
+}
+
+export default TodoList;
